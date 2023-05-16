850b1ce856e6e270b434fe54c1369fc90e71772e
Nick Gunn <nick@ausimian.net>
2023-05-16 11:25:59 +1000

    Add appup compiler

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Castle.MixProject do
   def project do
     [
       app: :castle,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
